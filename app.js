fcc83ef681344813ee2e24c06f68fb25cc614ba9
Christopher Bigelow <chrisbigelow25@gmail.com>
2017-12-03 20:43:45 -0800

    add express and mockups

--- app.js
+++ app.js
@@ -1,43 +1,94 @@
 let Twitter = require('twitter');
+let config = require('./config.js');
 const { getRequestBearer } = require('./request_bearer_token');
 
+let express = require('express');
+let app = express();
 
-let config = require('./config.js');
+let server = require('http').Server(app);
+let io = require('socket.io')(server);
 
-const makeTwitterRequest = () => {
+// app.use('/', express.static(__dirname + '/public'));
 
-  let twitterRequest = new Twitter(config);
+// let currentStream = 0;
+// let twitterRequest = new Twitter(config);
 
-  let searchParams = {
-    q: '#trump',
-    count: 100,
-    result_type: 'recent',
-    lang: 'en'
-  };
+// getRequestBearer((err,res) => {
+//   if (err) {
+//     return console.log("this is the error message:" + err);
+//   } else {
+//     config.bearer_token = res.access_token;
+//     makeSocketConnection();
 
-  twitterRequest.get('search/tweets', searchParams, function(err, data, response) {
-    if(!err) {
-      data.statuses.map((status, index) => {
-        console.log('-------------------------');
-        console.log(`Status #${index}: ` + status.text);
-        console.log('-------------------------');
+//   }
+// });
+
+// const makeSocketConnection = () => {
+
+//     io.on('connection', function (socket) {
+    
+//         socket.emit('init');
+    
+//         socket.on('query', function(query) {
+//             twitterRequest.stream('statuses/filter', {track: query}, function(stream) {
+//                 if (currentStream)
+//                     currentStream.destroy();
+//                 stream.on('data', function(tweet) {
+//                     socket.emit('tweet', tweet);
+//                 });
+//                 stream.on('error', function(error) {
+//                     console.log(error);
+//                 });
+//                 currentStream = stream;
+//             });
+//         });
+//     });
+// };
+
+app.get('/', function(req, res){
+
+
+    const makeTwitterRequest = () => {
+
+      let twitterRequest = new Twitter(config);
+
+      let searchParams = {
+        q: '#trump',
+        count: 100,
+        result_type: 'recent',
+        lang: 'en'
+      };
+
+      twitterRequest.get('search/tweets', searchParams, function(err, data, response) {
+        if(!err) {
+          res.send(data.statuses);
+        } else {
+          console.log(err);
+        }
       });
-    } else {
-      console.log(err);
-    }
-  });
 
-};
+    };
 
-getRequestBearer((err,res) => {
-  if (err) {
-    return console.log("this is the error message:" + err);
-  } else {
-    config.bearer_token = res.access_token;
-    makeTwitterRequest();
+    getRequestBearer((err,resp) => {
+      if (err) {
+        return console.log("this is the error message:" + err);
+      } else {
+        config.bearer_token = resp.access_token;
+        makeTwitterRequest();
 
-  }
+      }
+    });
+
 });
+
+server.listen(3000);
+
+
+// .map((status, index) => {
+//   console.log('-------------------------');
+//   console.log(`Status #${index}: ` + status.text);
+//   console.log('-------------------------');
+// });
 
 
 
